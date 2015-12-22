5d885a57c58ce91f59e00fbc47672541ef71f33c
Zelkjo Agic <zagic@zen.hum.ku.dk>
2015-12-22 14:14:48 +0100

    x
    Former-commit-id: 79461327a57d091bb52a5e3e1a1fa149534eac9e

--- run/project.sh
+++ run/project.sh
@@ -9,7 +9,7 @@ HOME=/home/zagic/nlpfromscratch
 for target in en; do
     for source in `cat $HOME/data/lists/sources.txt`; do
         if [ "$source" != "$target" ]; then
-            echo "python $HOME/src/projection/project.py --source $HOME/data/conll/${source}.tt.conll.out --target $HOME/data/conll/${target}.tt.conll.out --word_alignment $HOME/data/walign/${source}-${target}.wal --sentence_alignment $HOME/data/salign/${source}-${target}.sal --norm_before softmax --norm_after identity --binary --temperature 0.8 1> $HOME/data/projections/${source}-${target}.proj 2> $HOME/data/logs/${source}-${target}.proj.log" >> $HOME/run/commands_project.txt
+            echo "python $HOME/src/projection/project.py --source $HOME/data/conll/${source}.tt.conll.out --target $HOME/data/conll/${target}.tt.conll.out --word_alignment $HOME/data/walign/${source}-${target}.wal --sentence_alignment $HOME/data/salign/${source}-${target}.sal --norm_before identity --norm_after identity --binary --temperature 0.8 --trees 1> $HOME/data/projections/${source}-${target}.proj 2> $HOME/data/logs/${source}-${target}.proj.log" >> $HOME/run/commands_project.txt
         fi
     done
 
