445e5d67baa928a08a52a45377b79b1363938f73
Zeljko Agic <zeljko.agic@gmail.com>
2015-12-24 13:18:32 +0100

    x
    Former-commit-id: 81285e6862f218318855f5ef466cdb5e272e6f44

--- run/create_osl_files.sh
+++ run/create_osl_files.sh
@@ -3,7 +3,8 @@
 HOME=/home/zagic/nlpfromscratch
 # HOME=/Users/zagic/Work/cph/pycharm_projects/nlpfromscratch
 
-> $HOME/run/commands_create_osl_files.txt
+> $HOME/run/commands_create_osl_files_bible.txt
+> $HOME/run/commands_create_osl_files_watchtower.txt
 
 # merge lists of sources and targets into a list of all languages
 # if [ ! -f $HOME/data/lists/all_languages.txt ]; then
@@ -12,8 +13,8 @@ HOME=/home/zagic/nlpfromscratch
 
 # iterate through all language pairs
 for lang in `cat $HOME/data/lists/sources.txt`; do
-    echo "python $HOME/src/helper/conll2osl.py $HOME/data/conll/$lang.bible.svm_mira.conll 1> $HOME/data/raw/$lang.bible.osl 2> $HOME/data/logs/$lang.bible.osl.log"
-    echo "python $HOME/src/helper/conll2osl.py $HOME/data/conll/$lang.watchtower.svm_mira.conll 1> $HOME/data/raw/$lang.watchtower.osl 2> $HOME/data/logs/$lang.watchtower.osl.log"
+    echo "python $HOME/src/helper/conll2osl.py $HOME/data/conll/$lang.bible.svm_mira.conll 1> $HOME/data/raw/$lang.bible.osl 2> $HOME/data/logs/$lang.bible.osl.log" >> $HOME/run/commands_create_osl_files_bible.txt
+    echo "python $HOME/src/helper/conll2osl.py $HOME/data/conll/$lang.watchtower.svm_mira.conll 1> $HOME/data/raw/$lang.watchtower.osl 2> $HOME/data/logs/$lang.watchtower.osl.log" >> $HOME/run/commands_create_osl_files_watchtower.txt
 done
 
 # remove a temporary file
