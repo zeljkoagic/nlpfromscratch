b91032e2773173b5ddeb1e581e7106cf868347cb
Zeljko Agic <zeljko.agic@gmail.com>
2015-12-24 13:17:03 +0100

    x
    Former-commit-id: b32bc095e67fba740e353642670cba15fb4d4c50

--- run/salign.sh
+++ run/salign.sh
@@ -3,7 +3,8 @@
 HOME=/home/zagic/nlpfromscratch
 # HOME=/Users/zagic/Work/cph/pycharm_projects/nlpfromscratch
 
-> $HOME/run/commands_salign.txt
+> $HOME/run/commands_salign_bible.txt
+> $HOME/run/commands_salign_watchtower.txt
 
 # merge lists of sources and targets into a list of all languages
 # if [ ! -f $HOME/data/lists/all_languages.txt ]; then
@@ -15,7 +16,8 @@ for l1 in `cat $HOME/data/lists/sources.txt`; do
     for l2 in `cat $HOME/data/lists/sources.txt`; do
         if [ "$l1" != "$l2" ]; then
             # perform sentence alignment
-            echo "$HOME/tools/salign/hunalign-1.1/src/hunalign/hunalign -utf -bisent -cautious -realign /dev/null $HOME/data/raw/${l1}.osl $HOME/data/raw/${l2}.osl 1> $HOME/data/salign/${l1}-${l2}.sal 2> $HOME/data/logs/${l1}-${l2}.sal.log" >> $HOME/run/commands_salign.txt
+            echo "$HOME/tools/salign/hunalign-1.1/src/hunalign/hunalign -utf -bisent -cautious -realign /dev/null $HOME/data/raw/${l1}.bible.osl $HOME/data/raw/${l2}.bible.osl 1> $HOME/data/salign/${l1}-${l2}.bible.sal 2> $HOME/data/logs/${l1}-${l2}.bible.sal.log" >> $HOME/run/commands_salign_bible.txt
+            echo "$HOME/tools/salign/hunalign-1.1/src/hunalign/hunalign -utf -bisent -cautious -realign /dev/null $HOME/data/raw/${l1}.watchtower.osl $HOME/data/raw/${l2}.watchtower.osl 1> $HOME/data/salign/${l1}-${l2}.watchtower.sal 2> $HOME/data/logs/${l1}-${l2}.watchtower.sal.log" >> $HOME/run/commands_salign_watchtower.txt
         fi
     done
 done
