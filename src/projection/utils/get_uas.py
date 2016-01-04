dcbe42cbf1676fa856aba184cbe0c2fba1661bac
Zeljko Agic <zeljko.agic@gmail.com>
2016-01-04 16:09:15 +0100

    x
    Former-commit-id: 294fa75424294e2b2fd25601220bd629ca13a098

---
+++ src/projection/utils/get_uas.py
@@ -0,0 +1,19 @@
+import sys
+
+gold = open(sys.argv[1]).readlines()
+system = open(sys.argv[2]).readlines()
+
+correct = 0
+total = 0
+
+for g, s in zip(gold, system):
+    g = g.strip()
+    s = s.strip()
+    if g and s:
+        g = g.split()[6]
+        s = s.split()[6]
+        if g == s:
+            correct += 1
+        total += 1
+
+print(correct / total)
