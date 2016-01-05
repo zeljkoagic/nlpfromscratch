8efbb5217bb81e2865abbfe440982ff570ad4f62
Zeljko Agic <zeljko.agic@gmail.com>
2016-01-05 23:00:52 +0100

    x
    Former-commit-id: 779cdff244445e19e299883101f390e79e08a2dc

--- run/project.sh
+++ run/project.sh
@@ -11,7 +11,14 @@ for target in en; do
         for corpus in bible; do
             for algorithm in svm_mira; do
                 if [ "$source" != "$target" ]; then
-                    echo "python $HOME/src/projection/project.py --source $HOME/data/conll/${source}.$corpus.$algorithm.conll --target $HOME/data/conll/${target}.$corpus.$algorithm.conll --word_alignment $HOME/data/walign/${source}-${target}.$corpus.wal --sentence_alignment $HOME/data/salign/${source}-${target}.$corpus.sal --norm_before standardize --norm_after identity --binary --stop_after 5000 1> $HOME/data/projections/${source}-${target}.$corpus.$algorithm.proj 2> $HOME/data/logs/${source}-${target}.$corpus.$algorithm.proj.log" >> $HOME/run/commands_project_bible.txt
+                    echo "python $HOME/src/projection/project.py
+                    --source $HOME/data/conll/${source}.$corpus.$algorithm.conll
+                    --target $HOME/data/conll/${target}.$corpus.$algorithm.conll
+                    --word_alignment $HOME/data/walign/${source}-${target}.$corpus.wal
+                    --sentence_alignment $HOME/data/salign/${source}-${target}.$corpus.sal
+                    --norm_before standardize --norm_after identity --stop_after 5000
+                    1> $HOME/data/projections/${source}-${target}.$corpus.$algorithm.proj
+                    2> $HOME/data/logs/${source}-${target}.$corpus.$algorithm.proj.log" >> $HOME/run/commands_project_bible.txt
                 fi
             done
         done
--- run/walign.sh
+++ run/walign.sh
@@ -14,16 +14,17 @@ HOME=/home/zagic/nlpfromscratch
 # fi
 
 # iterate through all language pairs
-for l1 in `cat $HOME/data/lists/sources.txt`; do
-    for l2 in `cat $HOME/data/lists/sources.txt`; do
-        if [ "$l1" != "$l2" ]; then
-            # extract sentence pairs (does lowercasing too)
-            # currently supports only fast_align!
-            echo "python $HOME/src/helper/extract_sentence_pairs.py $HOME/data/raw/${l1}.bible.osl $HOME/data/raw/${l2}.bible.osl $HOME/data/salign/${l1}-${l2}.bible.sal 1> $HOME/data/walign/${l1}-${l2}.bible.spairs 2> $HOME/data/logs/${l1}-${l2}.bible.spairs.log" >> $HOME/run/commands_spair_bible.txt
-            echo "python $HOME/src/helper/extract_sentence_pairs.py $HOME/data/raw/${l1}.watchtower.osl $HOME/data/raw/${l2}.watchtower.osl $HOME/data/salign/${l1}-${l2}.watchtower.sal 1> $HOME/data/walign/${l1}-${l2}.watchtower.spairs 2> $HOME/data/logs/${l1}-${l2}.watchtower.spairs.log" >> $HOME/run/commands_spair_watchtower.txt
-            # perform word alignment
-            echo "$HOME/tools/walign/fast_align/fast_align -d -o -v -r -i $HOME/data/walign/${l1}-${l2}.bible.spairs 1> $HOME/data/walign/${l1}-${l2}.bible.wal 2> $HOME/data/logs/${l1}-${l2}.bible.wal.log" >> $HOME/run/commands_walign_bible.txt
-            echo "$HOME/tools/walign/fast_align/fast_align -d -o -v -r -i $HOME/data/walign/${l1}-${l2}.watchtower.spairs 1> $HOME/data/walign/${l1}-${l2}.watchtower.wal 2> $HOME/data/logs/${l1}-${l2}.watchtower.wal.log" >> $HOME/run/commands_walign_watchtower.txt
-        fi
+for corpus in bible; do
+    for l1 in `cat home/bplank/preprocess-holy-data/languages/trg-src-${corpus}.txt`; do
+        for l1 in `cat home/bplank/preprocess-holy-data/languages/trg-src-${corpus}.txt`; do
+            if [ "$l1" != "$l2" ]; then
+                # extract sentence pairs (does lowercasing too)
+                # currently supports only fast_align!
+                # echo "python $HOME/src/helper/extract_sentence_pairs.py $HOME/data/raw/${l1}.bible.osl $HOME/data/raw/${l2}.bible.osl $HOME/data/salign/${l1}-${l2}.bible.sal 1> $HOME/data/walign/${l1}-${l2}.bible.spairs 2> $HOME/data/logs/${l1}-${l2}.bible.spairs.log" >> $HOME/run/commands_spair_bible.txt
+                # echo "python $HOME/src/helper/extract_sentence_pairs.py $HOME/data/raw/${l1}.watchtower.osl $HOME/data/raw/${l2}.watchtower.osl $HOME/data/salign/${l1}-${l2}.watchtower.sal 1> $HOME/data/walign/${l1}-${l2}.watchtower.spairs 2> $HOME/data/logs/${l1}-${l2}.watchtower.spairs.log" >> $HOME/run/commands_spair_watchtower.txt
+                # perform word alignment
+                echo "$HOME/tools/walign/fast_align/fast_align -v -r -i /home/bplank/preprocess-holy-data/data/walign/${l1}-${l2}.${corpus}.spairs 1> $HOME/data/walign/${l1}-${l2}.${corpus}.wal 2> $HOME/data/logs/${l1}-${l2}.${corpus}.log" >> $HOME/run/commands_walign_${corpus}.txt
+            fi
+        done
     done
 done
