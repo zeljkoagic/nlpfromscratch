d4cea1c856a3d0ef6faee4fe2750aa2113036e66
Zeljko Agic <zeljko.agic@gmail.com>
2016-01-06 20:35:27 +0100

    x
    Former-commit-id: 9383c8c9ebea15a565ca6240e8d7ca02538bf1f7

--- run/project.sh
+++ run/project.sh
@@ -11,21 +11,23 @@ for corpus in bible; do
     for trees in 0 1; do
         for binary in 1; do
             for with_pp in 0; do
-                for target in `cat /home/bplank/preprocess-holy-data/languages/trg-src-${corpus}.txt`; do
-                    for source in `cat /home/bplank/preprocess-holy-data/languages/src-${corpus}.txt`; do
-                        for parser in svm_mira; do
-                            if [ "$source" != "$target" ]; then
-                                echo "python $HOME/src/projection/project.py" \
-                                "--source /home/bplank/multilingparse/data/unlab/tinytok/bible2project/$source.2proj.conll" \
-                                "--target /home/bplank/multilingparse/data/unlab/tinytok/bible2project/$target.2proj.conll" \
-                                "--word_alignment /home/bplank/preprocess-holy-data/data/walign/${source}-${target}.$corpus.reverse.wal" \
-                                "--sentence_alignment /home/bplank/preprocess-holy-data/data/salign/${source}-${target}.$corpus.sal" \
-                                "--norm_before standardize --norm_after softmax --binary $binary --trees $trees --with_pp $with_pp" \
-                                "--stop_after $stop" \
-                                "1> $HOME/data/projections/${source}-${target}.$corpus.$parser.trees_${trees}.binary_${binary}.with_pp_${with_pp}.ibm1.proj" \
-                                "2> $HOME/data/logs/${source}-${target}.$corpus.$parser.trees_${trees}.binary_${binary}.with_pp_${with_pp}.ibm1.proj.log" \
-                                >> $HOME/run/commands_project_${corpus}_ibm1.txt
-                            fi
+                for aligner in ibm1 ibm2; do
+                    for target in `cat /home/bplank/preprocess-holy-data/languages/trg-src-${corpus}.txt`; do
+                        for source in `cat /home/bplank/preprocess-holy-data/languages/src-${corpus}.txt`; do
+                            for parser in svm_mira; do
+                                if [ "$source" != "$target" ]; then
+                                    echo "python $HOME/src/projection/project.py" \
+                                    "--source /home/bplank/multilingparse/data/unlab/tinytok/bible2project/$source.2proj.conll" \
+                                    "--target /home/bplank/multilingparse/data/unlab/tinytok/bible2project/$target.2proj.conll" \
+                                    "--word_alignment /home/bplank/preprocess-holy-data/data/walign/${source}-${target}.$corpus.reverse.wal" \
+                                    "--sentence_alignment /home/bplank/preprocess-holy-data/data/salign/${source}-${target}.$corpus.sal" \
+                                    "--norm_before standardize --norm_after softmax --binary $binary --trees $trees --with_pp $with_pp" \
+                                    "--stop_after $stop" \
+                                    "1> $HOME/data/projections/${source}-${target}.$corpus.$parser.trees_${trees}.binary_${binary}.with_pp_${with_pp}.$aligner.proj" \
+                                    "2> $HOME/data/logs/${source}-${target}.$corpus.$parser.trees_${trees}.binary_${binary}.with_pp_${with_pp}.$aligner.proj.log" \
+                                    >> $HOME/run/commands_project_${corpus}_${aligner}.txt
+                                fi
+                            done
                         done
                     done
                 done
--- run/vote.sh
+++ run/vote.sh
@@ -10,15 +10,17 @@ for corpus in bible; do
     for trees in 0 1; do
         for binary in 1; do
             for with_pp in 0; do
-                for parser in svm_mira; do
-                    for target in `cat /home/bplank/preprocess-holy-data/languages/trg-src-${corpus}.txt`; do
-                        echo "python vote_pos_and_deps.py" \
-                        "--target /home/bplank/multilingparse/data/unlab/tinytok/bible2project/en.2proj.conll" \
-                        "--projections $HOME/data/projections/*-${target}.$corpus.$parser.trees_${trees}.binary_${binary}.with_pp_${with_pp}.proj" \
-                        "--decode --skip_untagged --stop_after $stop" \
-                        "1> $HOME/data/votes/$target.$corpus.$parser.trees_${trees}.binary_${binary}.with_pp_${with_pp}.vote" \
-                        "2> $HOME/data/logs/$target.$corpus.$parser.trees_${trees}.binary_${binary}.with_pp_${with_pp}.vote.log" \
-                        >> $HOME/run/commands_vote_${corpus}.txt
+                for aligner in ibm1 ibm2; do
+                    for parser in svm_mira; do
+                        for target in `cat /home/bplank/preprocess-holy-data/languages/trg-src-${corpus}.txt`; do
+                            echo "python vote_pos_and_deps.py" \
+                            "--target /home/bplank/multilingparse/data/unlab/tinytok/bible2project/en.2proj.conll" \
+                            "--projections $HOME/data/projections/*-${target}.$corpus.$parser.trees_${trees}.binary_${binary}.with_pp_${with_pp}.$aligner.proj" \
+                            "--decode --skip_untagged --stop_after $stop" \
+                            "1> $HOME/data/votes/$target.$corpus.$parser.trees_${trees}.binary_${binary}.with_pp_${with_pp}.$aligner.vote" \
+                            "2> $HOME/data/logs/$target.$corpus.$parser.trees_${trees}.binary_${binary}.with_pp_${with_pp}.$aligner.vote.log" \
+                            >> $HOME/run/commands_vote_${corpus}_${aligner}.txt
+                        done
                     done
                 done
             done
