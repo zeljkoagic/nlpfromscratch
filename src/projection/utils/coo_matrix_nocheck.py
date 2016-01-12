7dfd42754c574e96dc687d4e0bb4758e36a9d1a1
Anders Johannsen <anders@johannsen.com>
2016-01-12 18:26:28 +0100

    standardize method for sparse matrices
    Former-commit-id: 57a8f0582026ec1d572659dec2b0ebe6ebad5540

--- src/projection/utils/coo_matrix_nocheck.py
+++ src/projection/utils/coo_matrix_nocheck.py
@@ -6,3 +6,7 @@ class CooMatrix:
         self.col = np.array(col, dtype=np.int32)
         self.data = np.array(data)
         self.shape = shape
+
+    @classmethod
+    def standardize(cls, M):
+        M.data = (M.data - M.data.mean()) / M.data.std()
