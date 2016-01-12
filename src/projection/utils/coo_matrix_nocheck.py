a8c7b5b4bdc2cded694477abe81da640e38def13
Zeljko Agic <zeljko.agic@gmail.com>
2016-01-12 14:29:47 +0100

    x
    Former-commit-id: c6341f742678f0f31edca80efee6a27129418237

--- src/projection/utils/coo_matrix_nocheck.py
+++ src/projection/utils/coo_matrix_nocheck.py
@@ -2,7 +2,7 @@ import numpy as np
 
 class CooMatrix:
     def __init__(self, row, col, data, shape):
-        self.row = np.array(row, dtype=np.uint32)
-        self.col = np.array(col, dtype=np.uint32)
+        self.row = np.array(row, dtype=np.int32)
+        self.col = np.array(col, dtype=np.int32)
         self.data = np.array(data)
         self.shape = shape
