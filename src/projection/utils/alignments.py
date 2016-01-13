4b51090b59a91bd275890951b5df7f19d43e1c4a
Zeljko Agic <zeljko.agic@gmail.com>
2016-01-13 09:33:31 +0100

    x
    Former-commit-id: 2e5b5ce6ea5d18ef75cad3f0f9ad50579c37b3e4

--- src/projection/utils/alignments.py
+++ src/projection/utils/alignments.py
@@ -118,26 +118,6 @@ def project_dependencies_to_target(S, A):  # TODO Matrix S must be normalized, i
     return T
 
 
-def project_dependencies_to_target_new(S, A):
-    m_plus_one, n_plus_one = A.shape
-
-    T = np.ones(shape=(n_plus_one, n_plus_one)) * np.nan  # target graph
-    T_edge = np.ones_like(T) * np.nan
-
-    # for each dependent d in source graph (dependents are rows!)
-    rows = np.expand_dims(A, axis=-1)
-    targets_for_row = np.zeros([A.shape[0], T.shape[0], T.shape[0]])
-
-    for h in range(m_plus_one):
-        np.dot(rows, A[h].reshape(1, -1), out=targets_for_row)
-        targets_for_row *= S[:, h].reshape(-1, 1, 1)
-
-        np.nanmax(targets_for_row, axis=0, out=T_edge)
-        np.fmax(T, T_edge, out=T)
-
-    return T
-
-
 def project_token_labels(source_labels, wa_pairs, wa_probs, weigh_votes=True):
     """Projects the token labels from source to target tokens in a sentence.
 
@@ -148,8 +128,6 @@ def project_token_labels(source_labels, wa_pairs, wa_probs, weigh_votes=True):
     :return: a dictionary of vote counters, indexed by target token CoNLL id
     """
     label_votes = defaultdict(Counter)  # dictionary of counters
-
-    print(source_labels, wa_pairs)
 
     it = 0
     for sid, tid in wa_pairs:
