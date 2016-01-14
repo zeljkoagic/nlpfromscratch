corrected off-by-one bug in get_alignment_matrix
Former-commit-id: 059cdb4ad04d336e82d72340d328eb8519ff683a

src/projection/project.py
@@ -100,7 +100,7 @@ for target_sentence in conll.sentences(target_file_handle, sentence_getter=conll
 
     # now that the sentence ids match and word alignments are in place,
     # get the sentence, POS, and graph from the source
-    source_sentence, S_sparse, source_pos_tags = source_sentences[source_sid] # get_source_data(source_file_handle)
+    source_sentence, S_sparse, source_pos_tags = source_sentences[source_sid]  # get_source_data(source_file_handle)
 
     # source matrix normalization
     # S = np.full(S_sparse.shape, fill_value=np.nan)