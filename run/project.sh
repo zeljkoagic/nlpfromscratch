1f8009baa22708afcf9b7985c3d92ecd3538ca51
Zeljko Agic <zeljko.agic@gmail.com>
2016-01-14 15:06:38 +0100

    x
    Former-commit-id: f062ddc593ba18ae2ca93dfa66b6ef3ad64c0de7

--- run/project.sh
+++ run/project.sh
@@ -13,7 +13,7 @@ for corpus in bible watchtower; do
                 for aligner in ibm1 ibm2; do
                     for target in `cat /home/bplank/preprocess-holy-data/languages/trg-src-${corpus}.txt`; do
                         for source in `cat /home/bplank/preprocess-holy-data/languages/src-${corpus}.txt`; do
-                            for parser in svm_mira; do
+
                                 if [ "$source" != "$target" ]; then
                                     echo "python $HOME/src/projection/project.py" \
                                     "--source /home/bplank/parse-holy-data/data/2project/${corpus}/$source.2proj.conll" \
@@ -22,11 +22,11 @@ for corpus in bible watchtower; do
                                     "--sentence_alignment /home/bplank/preprocess-holy-data/data/salign/${source}-${target}.$corpus.sal" \
                                     "--norm_before standardize --norm_after identity --binary $binary --trees $trees" \
                                     "--use_similarity $similarity" \
-                                    "1> $HOME/data/projections/${source}-${target}.$corpus.$parser.trees_${trees}.binary_${binary}.with_pp_${with_pp}.$aligner.proj" \
-                                    "2> $HOME/data/logs/${source}-${target}.$corpus.$parser.trees_${trees}.binary_${binary}.with_pp_${with_pp}.$aligner.proj.log" \
+                                    "1> $HOME/data/projections/${source}-${target}.corpus_${corpus}.aligner_${aligner}.trees_${trees}.binary_${binary}.similarity_${similarity}.proj" \
+                                    "2> $HOME/data/logs/${source}-${target}.corpus_${corpus}.aligner_${aligner}.trees_${trees}.binary_${binary}.similarity_${similarity}.proj.log" \
                                     >> $HOME/run/commands_project_${corpus}_${aligner}.txt
                                 fi
-                            done
+
                         done
                     done
                 done
