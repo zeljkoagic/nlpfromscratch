83fa1c8206dad71606698f754908ae39cdd5b962
Zeljko Agic <zeljko.agic@gmail.com>
2016-01-15 10:45:06 +0100

    x
    Former-commit-id: a30110fe861add3d480f248aab3917f8269bdb77

--- run/vote.sh
+++ run/vote.sh
@@ -12,7 +12,7 @@ for corpus in bible watchtower; do
                             echo "python $HOME/src/projection/vote_pos_and_deps.py" \
                             "--target /home/bplank/parse-holy-data/data/2project/${corpus}/$target.2proj.conll" \
                             "--projections $HOME/data/projections/*-${target}.corpus_${corpus}.aligner_${aligner}.trees_${trees}.binary_${binary}.similarity_${similarity}.proj" \
-                            "unit_vote_pos $unitvote --skip_untagged --decode" \
+                            "--unit_vote_pos $unitvote --skip_untagged --decode" \
                             "1> $HOME/data/votes/${target}.corpus_${corpus}.aligner_${aligner}.trees_${trees}.binary_${binary}.similarity_${similarity}.unitvote_${unitvote}.vote" \
                             "2> $HOME/data/logs/${target}.corpus_${corpus}.aligner_${aligner}.trees_${trees}.binary_${binary}.similarity_${similarity}.unitvote_${unitvote}.vote.log" \
                             >> $HOME/run/commands_vote_${corpus}_${aligner}.txt
