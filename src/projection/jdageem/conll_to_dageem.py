866d9eb7d94b92bc63e51c48443eb7ac41f51f3e
Anders Johannsen <anders@johannsen.com>
2016-02-09 16:46:27 +0100

    Working ILP decoding. Not yet integrated with projection
    Former-commit-id: 78edb4e677085b58a01ec7652ffd5b3340bab409

--- src/projection/jdageem/conll_to_dageem.py
+++ src/projection/jdageem/conll_to_dageem.py
@@ -2,23 +2,31 @@ import argparse
 from pathlib import Path
 
 import conll
+from itertools import islice
 
 parser = argparse.ArgumentParser(description="Convert a CONLL file into a file ready for the `jdageem` software.")
 
 parser.add_argument("conll_file", type=Path)
+parser.add_argument("--heads", help="Output heads for evaluation", action='store_true')
+parser.add_argument("--stop_after", help="Only output this many", type=int)
+parser.add_argument("--length_cutoff", help="Supress outputting sentences longer than this cutoff", type=int)
 args = parser.parse_args()
 
 sent_id = 0
 with args.conll_file.open() as conll_file:
-    for tokens in conll.sentences(conll_file, conll.get_next_sentence):
-        forms = [token.form for token in tokens]
+    for tokens in islice(conll.sentences(conll_file, conll.get_next_sentence), args.stop_after):
+        tags = [token.cpos for token in tokens]
         heads = [str(token.head) for token in tokens]
-        assert len(forms) == len(heads)
+        assert len(tags) == len(heads)
 
-        if sent_id > 0:
+        if args.length_cutoff and len(tags) > args.length_cutoff:
+            continue
+
+        if sent_id > 0 and args.heads:
             print()
 
-        print(" ".join(forms))
-        print(" ".join(heads))
+        print(" ".join(tags))
+        if args.heads:
+            print(" ".join(heads))
 
         sent_id += 1
--- src/temp_test.py
+++ src/temp_test.py
@@ -4,34 +4,7 @@ import numpy as np
 from scipy import sparse
 
 
-def project_dependencies_to_target_new(S, A):  # TODO Matrix S must be normalized, i.e., negative values are not allowed!
-    """Projects source graph to target graph via source-target word alignment.
-
-    :param S: source graph (m+1 x m+1 matrix)
-    :param A: word alignment matrix (m+1 x n+1)
-    :return: target graph
-    """
-    m_plus_one, n_plus_one = A.shape
-
-    T = np.ones(shape=(n_plus_one, n_plus_one)) * np.nan  # target graph
-    T_edge = np.ones_like(T) * np.nan
-
-    # for each dependent d in source graph (dependents are rows!)
-    rows = np.expand_dims(A, axis=-1)
-    targets_for_row = np.zeros([A.shape[0], T.shape[0], T.shape[0]])
-
-    for h in range(m_plus_one):
-        np.dot(rows, A[h].reshape(1, -1), out=targets_for_row)
-        targets_for_row *= S[:, h].reshape(-1, 1, 1)
-
-
-        np.nanmax(targets_for_row, axis=0, out=T_edge)
-        np.fmax(T, T_edge, out=T)
-
-    return T
-
-
-def project_dependencies_to_target(S, A):  # TODO Matrix S must be normalized, i.e., negative values are not allowed!
+def project_dependencies_to_target(S, A):
     """Projects source graph to target graph via source-target word alignment.
 
     :param S: source graph (m+1 x m+1 matrix)
@@ -53,38 +26,31 @@ def project_dependencies_to_target(S, A):  # TODO Matrix S must be normalized, i
                 np.fmax(T, T_edge, out=T)
     return T
 
+def standardize(sentence_matrix):
+    normalized = sentence_matrix.copy()
+    normalized -= np.nanmean(normalized)
+    normalized /= np.nanstd(normalized)
 
+    return normalized
+
+def softmax(sentence_matrix, temperature=1.0):
+    """Softmax normalization.
+
+    :param sentence_matrix: (n+1 x n+1) weight matrix from the parser
+    :param temperature: softmax temperature
+    :return: softmaxed weight matrix
+    """
+    m_exp = np.exp(sentence_matrix/temperature)
+    return (m_exp.T / np.nansum(m_exp, axis=1)).T
 
 
 np.random.seed(40)
 S = np.random.random([40, 40])
 S = np.where(S > 0.5, S, 0)
 
-S_coo = sparse.coo_matrix(S)
-
 # A = np.random.random([40, 45])
 # A = np.where(A > 0.9, A, 0)
 A = np.diag(np.ones(45))[:40,:]
 
-A_coo = sparse.coo_matrix(A)
-A_csr = sparse.csr_matrix(A)
-
-
-import pyximport; pyximport.install()
-from project_deps import project_dependencies_fast, project_dependencies_faster
-
-variations = ["project_dependencies_fast(S, A_coo)",
-              "project_dependencies_faster(S_coo, A_csr)",
-              "project_dependencies_to_target(S, A)"
-              ]
-
-for variation in variations:
-    runs = 1000
-    elapsed = timeit.timeit(variation, number=runs, globals=globals())
-    secs_per_run = elapsed / runs
-
-    print("Running '{}'. Took {} microseconds per round".format(variation, secs_per_run * 1E6))
-
-
-
-
+proj_normed_softmax = softmax(project_dependencies_to_target(standardize(S), A))
+proj_softmax = softmax(project_dependencies_to_target(S, A))
