bac3e0946648e3daa05ff282c57be9bf72833f26
Zeljko Agic <zeljko.agic@gmail.com>
2016-02-18 17:09:52 +0100

    x
    Former-commit-id: fc5805a2c2798184618660be73d046eaa64ef08c

--- src/projection/parallel_sentence.py
+++ src/projection/parallel_sentence.py
@@ -2,13 +2,6 @@ from utils.coo_matrix_nocheck import CooMatrix
 from typing import List
 
 
-class ParallelSentence:
-    def __init__(self, target: List[str], sources: List[SourceSentence], gold=None):
-        self.sources = sources
-        self.target = target
-        self.gold = gold
-
-
 class SourceSentence:
     def __init__(self, weights: CooMatrix, pos: List[str], forms: List[str], language: str, alignments: List[tuple]):
         self.weights = weights
@@ -16,3 +9,10 @@ class SourceSentence:
         self.forms = forms
         self.language = language
         self.alignments = alignments
+
+
+class ParallelSentence:
+    def __init__(self, target: List[str], sources: List[SourceSentence], gold=None):
+        self.sources = sources
+        self.target = target
+        self.gold = gold
